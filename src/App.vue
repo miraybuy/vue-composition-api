258cc87efbc7576af5063f1f80ba81913951a396
Miray Buyukkaray <miraybuyukkaray@gmail.com>
2024-01-26 08:45:31 +0100

    composition api script setup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import CartItem from "./components/CartItem.vue";
+import useCart from "./useCart";
+
+const { items, addItem, removeItem } = useCart();
+
+addItem({
+  id: 1,
+  name: "Product X",
+  price: 20,
+  quantity: 1,
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <CartItem
+    v-for="item in items"
+    :cart-item="item"
+    :key="item.id"
+    @remove="removeItem"
+  />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
